6d332c04e54dc35920cefc7635e97b800eb384aa
Nikola Knezevic <nikola@knezevic.ch>
2025-09-17 23:37:25 +0200

    fix: additional fixes to ci

--- devenv.nix
+++ devenv.nix
@@ -21,26 +21,21 @@
   # scripts.hello.exec = "echo hello from $GREET";
 
   # https://devenv.sh/pre-commit-hooks/
-  git-hooks.hooks.shellcheck.enable = true;
   git-hooks.hooks.actionlint.enable = true;
+  # provided shellcheck has no options
+  git-hooks.hooks.shellcheck_custom = {
+    enable = true;
+    name = "shellcheck";
+    entry = "${pkgs.shellcheck}/bin/shellcheck -s bash";
+    types = [ "shell" ];
+    language = "system";
+  };
   # provided shfmt has no options...
   git-hooks.hooks.shfmt_custom = {
     enable = true;
-
-    # The name of the hook (appears on the report table):
     name = "shfmt";
-
-    # The command to execute (mandatory):
     entry = "${pkgs.shfmt}/bin/shfmt -d -i 4 -ci -bn -s";
-
-    # List of file types to run on (default: [ "file" ] (all files))
-    # see also https://pre-commit.com/#filtering-files-with-types
-    # You probably only need to specify one of `files` or `types`:
     types = [ "shell" ];
-
-    # The language of the hook - tells pre-commit
-    # how to install the hook (default: "system")
-    # see also https://pre-commit.com/#supported-languages
     language = "system";
   };
   # pre-commit.hooks.typos.enable = true;
